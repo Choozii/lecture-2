d41dbb27582cee3cbc825de935cb4caf34b504fa
Choozii <jiyechoo@gmail.com>
2024-04-24 19:48:49 +0900

    성능 개선

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
+import React, { Suspense, useState, lazy, useEffect } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+// import ImageModal from './components/ImageModal'
 
 function App() {
-    const [showModal, setShowModal] = useState(false)
+    const [showModal, setShowModal] = useState(false);
+    
+    useEffect(()=>{
+        const component = import('./components/ImageModal');
+        const img = new Image();
+        img.src = 'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800';
+    },[])
+    const ImageModal = lazy(()=>import('./components/ImageModal'))
 
     return (
         <div className="App">
@@ -16,7 +23,9 @@ function App() {
             <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
+            <Suspense fallback={<></>}>
             {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            </Suspense>
         </div>
     )
 }
